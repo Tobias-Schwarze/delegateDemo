d2744be19cff1d8c232945e177531a71e6425dd6
Skidi <skidi007@gmail.com>
2022-10-05 20:27:08 +0200

    sync

---
+++ WebAppDelegate/WebAppDelegate/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WebAppDelegate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get()
+        {
+            return String.Format("Hello User" + );
+        }
+    }
+}
